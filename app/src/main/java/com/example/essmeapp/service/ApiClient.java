82279c60a6edcde2ee7136a1f8c1437fef15e32e
hieuppham <phamtrunghieu6d@gmail.com>
2022-04-22 16:36:37 +0700

    api service

---
+++ app/src/main/java/com/example/essmeapp/service/ApiClient.java
@@ -0,0 +1,40 @@
+package com.example.essmeapp.service;
+
+import android.view.View;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static final String url = "https://essme.luanbt.live/api/";
+
+    private static Retrofit retrofit;
+
+
+    public static EssmeApi getAPI() {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .create();
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
+
+        if (retrofit == null)
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .client(okHttpClient)
+                    .build();
+        return retrofit.create(EssmeApi.class);
+    }
+}
