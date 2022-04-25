23c29e0d128cb18ef4cc18e494d572cceb8d1daf
Trinh Khanh Huyen <tkhuyen1212@gmail.com>
2022-04-25 09:30:00 +0700

    create activity fields_detail

--- app/src/main/java/com/example/essmeapp/service/ApiClient.java
+++ app/src/main/java/com/example/essmeapp/service/ApiClient.java
@@ -6,7 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
